4be6925e9b5ed42fa9060399c65d6b7c7b2f9d6d
DF <dmytro.furmanenko@gmail.com>
2024-10-18 22:07:45 +0300

    Lesson17

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,6 +13,7 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
+
     @AfterClass
     public void tearDown() {
         calculator = null;
@@ -22,6 +23,7 @@ public class CalculatorTest {
     @Test(description = "Перевірка методу додавання", priority = 1)
     public void testAddition() {
         int result = calculator.add(5, 3);
+        System.out.println("Результат додавання: " + result);
         Assert.assertEquals(result, 8);
     }
 
@@ -29,6 +31,7 @@ public class CalculatorTest {
     @Test(description = "Перевірка методу віднімання", priority = 2)
     public void testSubtraction() {
         int result = calculator.subtract(10, 5);
+        System.out.println("Результат віднімання: " + result);
         Assert.assertEquals(result, 5);
     }
 
@@ -36,6 +39,7 @@ public class CalculatorTest {
     @Test(description = "Перевірка методу множення", priority = 3)
     public void testMultiplication() {
         int result = calculator.multiply(4, 2);
+        System.out.println("Результат множення: " + result);
         Assert.assertEquals(result, 8);
     }
 
@@ -43,6 +47,7 @@ public class CalculatorTest {
     @Test(description = "Перевірка методу ділення", priority = 4)
     public void testDivision() {
         double result = calculator.divide(10, 2);
+        System.out.println("Результат ділення: " + result);
         Assert.assertEquals(result, 5.0);
     }
 
