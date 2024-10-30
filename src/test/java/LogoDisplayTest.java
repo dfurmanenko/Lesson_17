d7cd8bd90153458138c6413f46fcd8719fed63c8
DF <dmytro.furmanenko@gmail.com>
2024-10-30 19:59:38 +0200

    Lesson17

---
+++ src/test/java/LogoDisplayTest.java
@@ -0,0 +1,51 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LogoDisplayTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+
+        // Встановлюємо "explicit wait" тривалістю 10 секунд
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test(description = "Перевірка відображення логотипу на сторінці")
+    public void testLogoDisplay() {
+        // Знайти елемент логотипу за допомогою XPath
+        WebElement logo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class, 'header_logo')]")));
+
+        if (logo.isDisplayed()) {
+            System.out.println("Logo displayed");
+        } else {
+            System.out.println("Logo does not displayed");
+        }
+
+        // Додаткова перевірка результату
+        Assert.assertTrue(logo.isDisplayed(), "Логотип не відображається на сторінці.");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
